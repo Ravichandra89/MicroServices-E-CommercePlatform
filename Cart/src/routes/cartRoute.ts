93c20472235c145675080c82f4b38a5f2f1e30c9
Ravi chandra <githubravi01@gmail.com>
2024-11-22 02:02:59 +0530

    Routers for Cart

--- Cart/src/routes/cartRoute.ts
+++ Cart/src/routes/cartRoute.ts
@@ -1,8 +1,17 @@
 import express from "express";
-import {} from "../controller/cartController";
+import {
+  getProductFromCart,
+  addProductToCart,
+  deleteProductFromCart,
+  checkOut,
+} from "../controller/cart.controller";
 
 const cartRoute = express.Router();
 
 // Route enteries
+cartRoute.get("/", getProductFromCart);
+cartRoute.post("/", addProductToCart);
+cartRoute.delete("/:id", deleteProductFromCart);
+cartRoute.post("/checkout", checkOut);
 
 export default cartRoute;
